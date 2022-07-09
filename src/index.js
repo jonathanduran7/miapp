f0a6748bce8b86253927dde5bec1ffa7859b80c0
duranjonathan <joniduran624@gmail.com>
2022-07-09 13:03:50 -0300

    create docker file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res)=> {
-   res.send('mostrando data')
+   res.send('mostrando data desde node')
 })
 
 app.get('/crear', (req,res)=>{
